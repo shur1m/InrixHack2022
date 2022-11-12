085cad3c675e2c9bbb3bd4f5f53d7dd910347850
Shurim <ansondoeswork@gmail.com>
2022-11-12 10:10:32 -0800

    adding tailwind

--- inrix-hack-site/src/App.js
+++ inrix-hack-site/src/App.js
@@ -5,7 +5,7 @@ const name = 'Brad'
 function App() {
   return (
     <div className='container'>
-      <h1> Hello from React </h1>
+      <h1 className =''> Hello from React </h1>
       <h2> How are you doing, {name}</h2>
     </div>
   );
