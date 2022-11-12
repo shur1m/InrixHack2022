b85637f113678d8addb1c8a027848416f73677d2
James Vong <j.vong2020@gmail.com>
2022-11-12 12:03:20 -0800

    Create findroute API endpoint
    Use inrix api findRoute for obtaining route

--- inrix-backend/inrixAppAPIs.py
+++ inrix-backend/inrixAppAPIs.py
@@ -18,8 +18,26 @@ def find_lots():
     radius = request.args.get("radius", "300")
 
     # Ex: https://api.iq.inrix.com/lots/v3?point=37.74638779388551|-122.42209196090698&radius=300
-    params = {"point": f"{latitude}|{longitude}", "radius": radius}
+    params = {
+        "point": f"{latitude}|{longitude}",
+        "radius": radius
+    }
     url = 'https://api.iq.inrix.com/lots/v3'
     res = inrix_requests.get(url, params)
+    return jsonify(res)
 
+
+@inrix_page.route("/findroute")
+def find_route():
+    wp_1 = request.args.get("wp_1")
+    wp_2 = request.args.get("wp_2")
+
+    params = {
+        "wp_1": f"{wp_1}",
+        "wp_2": f"{wp_2}",
+        "format": "json"
+    }
+
+    url = "https://api.iq.inrix.com/findRoute"
+    res = inrix_requests.get(url, params)
     return jsonify(res)
