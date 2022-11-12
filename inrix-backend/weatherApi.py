12e1f7d9add6375fa1455c4acdc37f766f62786d
Arnav Choudhury <66573183+8coolguy@users.noreply.github.com>
2022-11-12 13:06:51 -0800

    weather api
    gets weather api for long and lat

--- inrix-backend/weatherApi.py
+++ inrix-backend/weatherApi.py
@@ -1,16 +1,21 @@
-from flask import Blueprint
+from flask import Blueprint, request,jsonify
 import requests
 
 
 weather = Blueprint('weather', __name__)
+WMO ={0:"Clear Sky",1:"Mainly Clear",2:"Partly Cloudly",3:"Overcast",45:"Fog",48:"Depositing Rime Fog",51:"Light Drizzle",53:"Moderate Drizzle",53:"Dense Drizzle",56:"Freezing Drizzle",57:"Freezing Drizzle",61:"Rain",63:"Rain",65:"Rain",66:"Rain",67:"Rain"}
 
 @weather.route('/weather')
 def getWeather():
     #https://api.open-meteo.com/v1/forecast?latitude=55.6763&longitude=12.5681&hourly=temperature_2m
     params=dict()
-    params['longitude']=requests.args.get('lon')
-    params['latitude']=requests.args.get('lat')
-    params['hourly']='temperature_2m'
-    query=requests.get(f'https://api.open-meteo.com/v1/forecast', params=params)
+    params['longitude']=request.args.get('lon')
+    params['latitude']=request.args.get('lat')
+    params["current_weather"]=True
+    params["temperature_unit"]="fahrenheit"
+    result=requests.get('https://api.open-meteo.com/v1/forecast',params).json()
+    result["current_weather"]["weathercode"]=WMO[result["current_weather"]["weathercode"]]
     
-    return query
+
+
+    return result
