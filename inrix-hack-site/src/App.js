a6b68a4ec9a68d763b56981b83ec3c7df3d7574f
Shurim <ansondoeswork@gmail.com>
2022-11-12 09:21:35 -0800

    initial commit

---
+++ inrix-hack-site/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+const name = 'Brad'
+
+function App() {
+  return (
+    <div className='container'>
+      <h1> Hello from React </h1>
+      <h2> How are you doing, {name}</h2>
+    </div>
+  );
+}
+
+export default App;
