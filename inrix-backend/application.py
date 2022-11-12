338b3378401a353f02b4b55a31227a61ee09e283
Arnav Choudhury <66573183+8coolguy@users.noreply.github.com>
2022-11-12 12:16:15 -0800

    Update application.py

--- inrix-backend/application.py
+++ inrix-backend/application.py
@@ -2,11 +2,13 @@ from flask import Flask
 
 from noiseApi import noise
 from inrixAppAPIs import inrix_page
+from weatherApi import weather
 
 app = Flask(__name__)
 
 app.register_blueprint(noise, url_prefix="/noise")
 app.register_blueprint(inrix_page, url_prefix="/inrix")
+app.register_blueprint(weather,url_prefix='/weather')
 
 
 @app.route("/")
