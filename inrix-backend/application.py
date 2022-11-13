2fced605f8792f3c56dfac1a656f0dfb4bc69a6f
James Vong <j.vong2020@gmail.com>
2022-11-12 18:41:37 -0800

    Update application.py

--- inrix-backend/application.py
+++ inrix-backend/application.py
@@ -19,7 +19,8 @@ app.register_blueprint(besttime_page, url_prefix='/besttime')
 def home():
     return "A group's Inrix API Hackathon Home Page"
 
-#distance , travelTime, indor,outdoor,Location,address
+
+# distance , travelTime, indor,outdoor,Location,address
 @app.route("/places")
 def places():
     with open("./ConfigFiles/locations.json", "r") as fp:
@@ -28,19 +29,19 @@ def places():
     return jsonify(res)
 
 
-#\d{1,5}\s\w.\s(\b\w*\b\s){1,2}\w*\.
+# \d{1,5}\s\w.\s(\b\w*\b\s){1,2}\w*\.
 @app.route("/txtToPoint")
-def addressToPoint():
-    address=request.args.get("searchText")
+def address_to_point():
+    address = request.args.get("searchText")
     with open("./ConfigFiles/ptv.json", "r") as fp:
         res = json.loads(fp.read())
 
-    params={"apiKey":res["apikey"],"searchText": address}
+    params = {"apiKey": res["apikey"], "searchText": address}
 
-    url="https://api.myptv.com/geocoding/v1/locations/by-text"
+    url = "https://api.myptv.com/geocoding/v1/locations/by-text"
 
-    result=requests.get(url,params=params)
-    if (len(result.json()['locations'])==0):
+    result = requests.get(url, params=params)
+    if len(result.json()['locations']) == 0:
         return jsonify("No Result")
-    l=min(len(result.json()['locations']),10)
-    return jsonify(result.json()['locations'][:l])
+
+    return jsonify(result.json()['locations'][:10])
